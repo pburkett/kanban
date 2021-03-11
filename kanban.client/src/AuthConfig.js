963f9c382e5c4c902ff292349c35cd7b31f22a59
pburkett <pburkett636@gmail.com>
2021-03-11 14:08:15 -0700

    client authconfig

--- kanban.client/src/AuthConfig.js
+++ kanban.client/src/AuthConfig.js
@@ -1,3 +1,4 @@
 export const domain = 'dev-jxvfv4ks.us.auth0.com'
-export const audience = 'https://kanban.com'
-export const clientId = 'KnpILWw8BQItR2lCp8RAQ8ThvhU34CWE'
+export const audience = 'https://generic'
+export const clientId = 'u8ZSX5RISvlg7hDjkkLss9QrEFo0BX04'
+'
